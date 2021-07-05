61cb50685880e5323602cab0db2799e4ddff20a7
Max <maximpalaguta@gmail.com>
2021-07-05 09:53:04 +0300

    Added and adapted section <<logos>>

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,8 @@
 window.addEventListener('DOMContentLoaded', () => {
+    //menu
     const menu = document.querySelector('.menu'),
-    menuItem = document.querySelectorAll('.menu__item'),
-    hamburger = document.querySelector('.hamburger');
+        menuItem = document.querySelectorAll('.menu__item'),
+        hamburger = document.querySelector('.hamburger');
 
     hamburger.addEventListener('click', () => {
         hamburger.classList.toggle('hamburger_active');
@@ -14,4 +15,15 @@ window.addEventListener('DOMContentLoaded', () => {
             menu.classList.toggle('menu_active');
         })
     })
+
+    //add logos
+
+    const logosParent = document.querySelector('.logos__wrapper');
+    for (let i = 1; i <= 18; i++) {
+        const img = document.createElement('img');
+        img.src = `icons/logos/${i}.svg`;
+        img.alt = 'icon';
+        img.classList.add('logos__icon');
+        logosParent.append(img);
+    }
 })
